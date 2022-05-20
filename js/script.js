a97600c7e064a90d2e6c27c97076d49e57bca5c7
javoxpl <liacovone7@outlook.it>
2022-05-20 16:24:00 +0200

    stampa numeri da 1 a 100

--- js/script.js
+++ js/script.js
@@ -7,3 +7,9 @@ MILESTONE 2
 Dato un container nel DOM, appendi un elemento html con il numero o la stringa corretta.
 MILESTONE 3
 Applica uno stile differente a seconda del valore dell’indice per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5.*/
+
+
+for (let number = 0; number <= 100; number++) {
+    console.log (number);
+    
+}
